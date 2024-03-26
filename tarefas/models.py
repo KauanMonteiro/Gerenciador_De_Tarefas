97c5348e24f21f1de60774146c8e26de96efcbdd
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-03-26 09:47:47 -0300

    models, criação da tarefa.html e css

--- tarefas/models.py
+++ tarefas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Tarefas(models.Model):
      titulo = models.CharField(max_length=100)
+     autor = models.CharField(max_length=50)
      descricao = models.TextField()
      data_criacao = models.DateTimeField(auto_now_add=True)
      data_limite = models.DateTimeField(auto_now_add=False)
