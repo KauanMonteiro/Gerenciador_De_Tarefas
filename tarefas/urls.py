ade33ac1a2edc3607c63ebe4a861a82ad485e46c
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-04-01 11:07:02 -0300

    Implementada a funcionalidade de marcar uma tarefa como concluída. Agora, ao clicar no botão 'Finalizar', a tarefa é marcada como concluída e a página é redirecionada para a lista de tarefas. Além disso, atualizado o template HTML das tarefas para exibir corretamente o status de conclusão. A mensagem 'Finalizada' agora é mostrada quando uma tarefa está marcada como concluída.

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tarefa/<int:id>/', views.tarefa_detail, name='tarefa_detail'),
+    path('tarefa/<int:id>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
 ]
+
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , redirect
+from django.urls import reverse 
 from .models import Tarefas
 
 def home(request):
-    tarefas_incompletas = Tarefas.objects.filter(concluida=False)
+    tarefas_incompletas = Tarefas.objects.all()
     return render(request, 'tarefas/pages/home.html', {'tarefas': tarefas_incompletas})
 
 def tarefa_detail(request, id):
     tarefa = get_object_or_404(Tarefas, pk=id)
     return render(request, 'tarefas/pages/tarefa_detalhe.html', {'tarefa': tarefa, 'is_detail_page': True})
+
+def concluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    if not tarefa.concluida:
+        tarefa.concluida = True
+        tarefa.save()
+    return redirect(reverse('home'))
