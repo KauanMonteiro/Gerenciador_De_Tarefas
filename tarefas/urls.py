9b731983fe6f98ce9ddc712f645aa8c8b4080211
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-04-01 11:24:54 -0300

    Adicionada função area_usuario para exibir todas as tarefas do usuário na área de usuário.

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('tarefa/<int:id>/', views.tarefa_detail, name='tarefa_detail'),
     path('tarefa/<int:id>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
-]
+    path('area_usuario/', views.area_usuario, name='area_usuario'),
+    ]
 
--- tarefas/views.py
+++ tarefas/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from .models import Tarefas
 
 def home(request):
-    tarefas_incompletas = Tarefas.objects.all()
+    tarefas_incompletas = Tarefas.objects.filter(concluida=False)
     return render(request, 'tarefas/pages/home.html', {'tarefas': tarefas_incompletas})
 
 def tarefa_detail(request, id):
@@ -16,3 +16,8 @@ def concluir_tarefa(request, id):
         tarefa.concluida = True
         tarefa.save()
     return redirect(reverse('home'))
+
+
+def area_usuario(request):
+    tarefas = Tarefas.objects.all()
+    return render(request, 'tarefas/pages/area_usuario.html', {'tarefas': tarefas})
