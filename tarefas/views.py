e8c53daa09d29ca8eb39b760af35ee23b842abf3
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-04-03 10:21:23 -0300

    Corrigido erro de NoReverseMatch relacionado ao URL de login.

--- tarefas/views.py
+++ tarefas/views.py
@@ -3,14 +3,20 @@ from django.urls import reverse
 from .models import Tarefas
 
 def home(request):
+    if 'usuario' not in request.session:
+        return redirect('login')
     tarefas_incompletas = Tarefas.objects.filter(concluida=False)
     return render(request, 'tarefas/pages/home.html', {'tarefas': tarefas_incompletas})
 
 def tarefa_detail(request, id):
+    if 'usuario' not in request.session:
+        return redirect('login')
     tarefa = get_object_or_404(Tarefas, pk=id)
     return render(request, 'tarefas/pages/tarefa_detalhe.html', {'tarefa': tarefa, 'is_detail_page': True})
 
 def concluir_tarefa(request, id):
+    if 'usuario' not in request.session:
+        return redirect('login')
     tarefa = get_object_or_404(Tarefas, pk=id)
     if not tarefa.concluida:
         tarefa.concluida = True
@@ -19,5 +25,7 @@ def concluir_tarefa(request, id):
 
 
 def area_usuario(request):
+    if 'usuario' not in request.session:
+        return redirect('login')
     tarefas = Tarefas.objects.all()
     return render(request, 'tarefas/pages/area_usuario.html', {'tarefas': tarefas})
