f8e4cf02aebc4ad0f8b776663e1b6fdc9ef4c414
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-05-18 15:06:25 -0300

    Adiciona suporte para tarefas de múltipla escolha no modelo Tarefas
    - Adicionado campos ao modelo Tarefas para armazenar possíveis respostas.
    - Adicionado campo para armazenar a resposta correta.
    
    - Aprimora formulário de criação de tarefa em HTML para incluir campos para respostas de múltipla escolha e um menu suspenso para selecionar a resposta correta.

--- tarefas/models.py
+++ tarefas/models.py
@@ -9,13 +9,19 @@ class Equipe(models.Model):
           return self.nome_equipe
 
 class Tarefas(models.Model):
-     titulo = models.CharField(max_length=100)
-     autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tarefas_autor')
-     descricao = models.TextField('')
-     data_criacao = models.DateTimeField(auto_now_add=True)
-     data_limite = models.DateTimeField(auto_now_add=False)
-     concluida = models.ManyToManyField(Usuario, blank=True, null=True)
-     tarefa_para = models.ManyToManyField(Equipe, null= False)
-
-     def __str__(self):
-          return self.titulo
+    titulo = models.CharField(max_length=100)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tarefas_autor')
+    descricao = models.TextField(null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_limite = models.DateTimeField(null=True)
+    concluida = models.ManyToManyField(Usuario, blank=True)
+    tarefa_para = models.ManyToManyField(Equipe)
+    alternativa1 = models.TextField(null=True)
+    alternativa2 = models.TextField(null=True)
+    alternativa3 = models.TextField(null=True)
+    alternativa4 = models.TextField(null=True)
+    alternativa5 = models.TextField(null=True)
+    alternativa_correta = models.IntegerField(choices=[(1, 'A'), (2, 'B'), (3, 'C'), (4, 'D'), (5, 'E')], null=True)
+    
+    def __str__(self):
+        return self.titulo
